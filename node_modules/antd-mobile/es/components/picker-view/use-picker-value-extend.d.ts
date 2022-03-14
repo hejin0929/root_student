bbff470e9ab2ea77744387c51a0156a1ae706326
黄河金 <myhuanghejin@163.com>
2022-03-14 16:29:29 +0800

    1

--- node_modules/antd-mobile/es/components/picker-view/use-picker-value-extend.d.ts
+++
@@ -1,2 +0,0 @@
-import { PickerValue, PickerValueExtend, PickerColumnItem } from './picker-view';
-export declare function usePickerValueExtend(columns: PickerColumnItem[][]): (val: PickerValue[]) => PickerValueExtend;
