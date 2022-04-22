0692d8c1bd75e8ace06e93530069a53108ec67df
huanghejin <huanghejin@realibox.com>
2022-04-22 10:24:35 +0800

    提交项目自适应功能

--- vite.config.ts
+++ vite.config.ts
@@ -27,4 +27,27 @@ export default defineConfig({
       "@widgets": path.resolve(__dirname, "src/widgets"),
     },
   },
+  css: {
+    postcss: {
+      plugins: [
+        require("autoprefixer"),
+        require("postcss-px-to-viewport")({
+          unitToConvert: "px",
+          viewportWidth: 375,
+          unitPrecision: 3,
+          propList: ["*"],
+          viewportUnit: "vw",
+          fontViewportUnit: "vw",
+          selectorBlackList: ["ignore-"],
+          minPixelValue: 1,
+          mediaQuery: false,
+          replace: true,
+          exclude: [],
+          landscape: true,
+          landscapeUnit: "vw",
+          landscapeWidth: 750,
+        }),
+      ],
+    },
+  },
 });
